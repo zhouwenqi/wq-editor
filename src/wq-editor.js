26a9e6a978ecc980ee72601a4f5798db4d12831a
zhouwenqi <zhouwenqi1982@live.com>
2018-04-26 14:33:38 +0800

    增加过滤目录

---
+++ src/wq-editor.js
@@ -0,0 +1,7 @@
+/**
+ * Copyright (c) 2018-present,zhouwenqi
+ * All rights reserved.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Editor,EditorState} from 'draft-js';
